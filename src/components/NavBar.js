236c1f819286963d483a170ad29de959d4d82a04
Thomas Santos <thomasbsjf@gmail.com>
2019-02-12 13:14:50 -0200

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const NavBar = () =>{
+    return(
+        <div>
+            <AppBar position="static">
+                <ToolBar>
+                    <Typography variant="title" color="inherit">
+                        Tela Protótipo 
+                    </Typography>
+                </ToolBar>
+            </AppBar>
+        </div>
+    )
+}
+export default NavBar;
