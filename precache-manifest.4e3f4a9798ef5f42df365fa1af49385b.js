58e84d3a95d40999b05a5a569ed0c1b8d431c1a5
Igor Knop <igorknop@gmail.com>
2019-02-19 19:23:22 -0300

    Updates

---
+++ precache-manifest.4e3f4a9798ef5f42df365fa1af49385b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e911a94cd3ad1538a52a",
+    "url": "/reader-game-client/static/js/runtime~main.e911a94c.js"
+  },
+  {
+    "revision": "ba81e8f8afaeeb923790",
+    "url": "/reader-game-client/static/js/main.ba81e8f8.chunk.js"
+  },
+  {
+    "revision": "c6c4875bc7e5729de73f",
+    "url": "/reader-game-client/static/js/2.c6c4875b.chunk.js"
+  },
+  {
+    "revision": "ba81e8f8afaeeb923790",
+    "url": "/reader-game-client/static/css/main.0228c9aa.chunk.css"
+  },
+  {
+    "revision": "11fc06dd30e887ae8d9098aaab78d1b7",
+    "url": "/reader-game-client/index.html"
+  }
+];
