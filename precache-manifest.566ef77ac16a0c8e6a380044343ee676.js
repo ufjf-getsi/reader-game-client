140778efa84c2eecbb2ed090cbd7e14f3d2647e9
Thomas Santos <thomasbsjf@gmail.com>
2019-04-01 22:11:36 -0300

    Updates

--- precache-manifest.566ef77ac16a0c8e6a380044343ee676.js
+++ precache-manifest.566ef77ac16a0c8e6a380044343ee676.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/reader-game-client/static/js/runtime~main.e911a94c.js"
   },
   {
-    "revision": "e7ab052533a8bd755fef",
-    "url": "/reader-game-client/static/js/main.e7ab0525.chunk.js"
+    "revision": "d4980d78a3f1262c7808",
+    "url": "/reader-game-client/static/js/main.d4980d78.chunk.js"
   },
   {
     "revision": "ce162a2adc7a172f0292",
     "url": "/reader-game-client/static/js/2.ce162a2a.chunk.js"
   },
   {
-    "revision": "e7ab052533a8bd755fef",
+    "revision": "d4980d78a3f1262c7808",
     "url": "/reader-game-client/static/css/main.a7ecb015.chunk.css"
   },
   {
-    "revision": "10406331b4445ee2db229561510f166e",
+    "revision": "30a19c896aee58bb49c396c67ac36b6f",
     "url": "/reader-game-client/index.html"
   }
 ];
