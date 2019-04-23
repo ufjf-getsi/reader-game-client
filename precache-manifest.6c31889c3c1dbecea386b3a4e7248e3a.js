b9fbf0c90884afe63d37f1d45971aeb87553accb
Igor Knop <igorknop@gmail.com>
2019-04-23 01:04:45 -0300

    Updates

---
+++ precache-manifest.6c31889c3c1dbecea386b3a4e7248e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a58eda547a6e2de0d0a7bb242fe71248",
+    "url": "/reader-game-client/static/media/GALAXY READER.a58eda54.png"
+  },
+  {
+    "revision": "e911a94cd3ad1538a52a",
+    "url": "/reader-game-client/static/js/runtime~main.172eafcf.js"
+  },
+  {
+    "revision": "1b0d2453e36ef9c1b598",
+    "url": "/reader-game-client/static/js/main.6548aa47.chunk.js"
+  },
+  {
+    "revision": "bc6b99d84899aac9b032",
+    "url": "/reader-game-client/static/js/2.61b551d8.chunk.js"
+  },
+  {
+    "revision": "1b0d2453e36ef9c1b598",
+    "url": "/reader-game-client/static/css/main.eaf9e19b.chunk.css"
+  },
+  {
+    "revision": "719aa4d1b582501670d175570d904044",
+    "url": "/reader-game-client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reader-game-client/precache-manifest.a450bca2a35f0ce805398c2fc760bd88.js"
+  "/reader-game-client/precache-manifest.6c31889c3c1dbecea386b3a4e7248e3a.js"
 );
 
 workbox.clientsClaim();
